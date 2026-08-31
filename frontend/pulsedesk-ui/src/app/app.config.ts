bbc1feccbf5dcf4c27b875e850df21d1f957b15d
Bruna Daher de Albuquerque <brunadaher@icloud.com>
2026-08-31 14:55:30 -0300

    Adds dark mode

--- frontend/pulsedesk-ui/src/app/app.config.ts
+++ frontend/pulsedesk-ui/src/app/app.config.ts
@@ -15,7 +15,10 @@ export const appConfig: ApplicationConfig = {
     providePrimeNG({
       license: "eyJpZCI6ImQzMDQzNjM3LWU4YWMtNGQyZC05MjE5LTUzYjhhZDRmODdlYiIsInByb2R1Y3QiOiJwcmltZXVpIiwidGllciI6ImNvbW11bml0eSIsInR5cGUiOiJkZXYiLCJpYXQiOjE3ODY3MzI5NDYsImV4cCI6MTgxODI2ODk0Nn0.kaATZKFrswesNqG57o3f-iCIfNux06PNEGwDvOb2fBMTNnlpYAJhFNZdbuFia-1awXflw_y9evmIxwZ-AGYZBQ",
       theme: {
-        preset: Aura
+        preset: Aura,
+        options: {
+          darkModeSelector: 'system'
+        }
       }
     })
   ]
